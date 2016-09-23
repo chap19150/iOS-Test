f9387de39a804906efcf1c1a956307722f3221ac
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-09-23 17:10:25 -0400

    Updated tableview settings

--- StrivrTest/StrivrTest/AuthorInfoViewController.swift
+++ StrivrTest/StrivrTest/AuthorInfoViewController.swift
@@ -9,31 +9,31 @@
 import UIKit
 
 class AuthorInfoViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
-    @IBOutlet weak var authorTableView: UITableView!
+    
+    @IBOutlet weak var commitsByAuthorTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.authorTableView.dataSource = self
-        self.authorTableView.delegate = self
-
+        self.commitsByAuthorTableView.delegate = self
+        self.commitsByAuthorTableView.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: AuthorInfoCustomTableViewCell.cellReuseIdentifier, for: indexPath)
-     
+        let cell = tableView.dequeueReusableCell(withIdentifier: AuthorInfoCustomTableViewCell.cellReuseIdentifier, for: indexPath) as! AuthorInfoCustomTableViewCell
+        
         return cell
     }
     
@@ -44,15 +44,15 @@ class AuthorInfoViewController: UIViewController, UITableViewDataSource, UITable
         
         return tableView.estimatedRowHeight
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
