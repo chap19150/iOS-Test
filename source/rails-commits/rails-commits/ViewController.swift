d90924072971919e970ab99a1bab3a055830c402
Jeff-DS <jeff.spingeld@gmail.com>
2016-10-22 19:34:15 -0400

    Set up API Client

--- source/rails-commits/rails-commits/ViewController.swift
+++ source/rails-commits/rails-commits/ViewController.swift
@@ -8,13 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var authors:[Author] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Access the GitHub API and get a list of recent commits to the rails repo.
+        APIClient().getRailsCommits(number: 30) { (authors) in
+            
+            self.authors = authors
+            
+        }
     }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.authors.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.authors[section].commits!.count
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
