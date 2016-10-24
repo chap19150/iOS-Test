57de5911725e6c22bda70ec2348a8a4228f527cb
Jeff-DS <jeff.spingeld@gmail.com>
2016-10-23 21:38:09 -0400

    Set up basic cell for commits

--- source/rails-commits/rails-commits/ViewController.swift
+++ source/rails-commits/rails-commits/ViewController.swift
@@ -15,16 +15,31 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Register nib
+        tableView.register(UINib(nibName: "commitCell", bundle: nil), forCellReuseIdentifier: "cell")
+        
         // Access the GitHub API and get a list of recent commits to the rails repo.
         APIClient().getRailsCommits(number: 30) { (authors) in
             
             self.authors = authors
+            self.tableView.reloadData()
             
         }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        
+        // Get a cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! commitCell
+        // Find the commit
+        let commit = authors[indexPath.section].commits?[indexPath.row]
+        // Set the cell properties
+        cell.timeLabel.text = commit!.timestamp
+        cell.messageTextView.text = commit!.message
+        cell.authorLabel.text = commit!.author
+        cell.avatarView.image = UIImage(named: "test")
+        
+        return cell
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
