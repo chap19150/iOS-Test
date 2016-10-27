4a90e4b9e205322b28edd709d27f682e92b46195
Hailey Sholty <haileysholty21@aol.com>
2016-10-26 21:46:39 -0400

    Remove duplicated directory

--- Pods/Target Support Files/Pods-StrivrTest/Pods-StrivrTest-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_StrivrTestVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_StrivrTestVersionString[];
-
--- Pods/Target Support Files/Pods-StrivrTestUITests/Pods-StrivrTestUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_StrivrTestUITests : NSObject
-@end
-@implementation PodsDummy_Pods_StrivrTestUITests
-@end
